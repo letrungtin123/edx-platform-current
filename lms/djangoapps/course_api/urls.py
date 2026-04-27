b4dadc407df9dcd1320d58a08043d388dc52bf45
letrungtin123 <lttvn917@gmail.com>
2026-04-27 14:20:21 +0700

    Initial commit

---
+++ lms/djangoapps/course_api/urls.py
@@ -0,0 +1,17 @@
+"""
+Course API URLs
+"""
+
+
+from django.conf import settings
+from django.urls import include, path, re_path
+
+from .views import CourseDetailView, CourseIdListView, CourseListView, CourseMentorsView
+
+urlpatterns = [
+    path('v1/courses/', CourseListView.as_view(), name="course-list"),
+    re_path(fr'^v1/courses/{settings.COURSE_KEY_PATTERN}/mentors/?$', CourseMentorsView.as_view(), name="course-mentors"),
+    re_path(fr'^v1/courses/{settings.COURSE_KEY_PATTERN}', CourseDetailView.as_view(), name="course-detail"),
+    path('v1/course_ids/', CourseIdListView.as_view(), name="course-id-list"),
+    path('', include('lms.djangoapps.course_api.blocks.urls'))
+]
